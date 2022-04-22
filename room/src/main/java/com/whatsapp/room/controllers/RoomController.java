1d7c69f5f28fcf3a23128cf94f29f0daa3d9bb38
AyushDocs <ayushalokdubey@gmail.com>
2022-04-23 03:35:10 +0530

    fixed room_service_requests not working

--- room/src/main/java/com/whatsapp/room/controllers/RoomController.java
+++ room/src/main/java/com/whatsapp/room/controllers/RoomController.java
@@ -3,18 +3,26 @@ package com.whatsapp.room.controllers;
 import com.whatsapp.room.dto.FindRoomsResponse;
 import com.whatsapp.room.dto.SaveRoomRequest;
 import com.whatsapp.room.service.RoomService;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/rooms")
+@RestController
 public class RoomController {
       private final RoomService roomService;
-
-      public void save(SaveRoomRequest saveRoomRequest) {
+      @PostMapping("/")
+      public void save(@RequestBody SaveRoomRequest saveRoomRequest) {
             roomService.saveRoom(saveRoomRequest);
       }
-
-      public FindRoomsResponse[] findRoomsWithUnreadMessagesByUserUuid(String userUuid) {
+      @GetMapping("/{userUuid}")
+      public FindRoomsResponse[] findRoomsWithUnreadMessagesByUserUuid(@PathVariable String userUuid) {
             return roomService.findRoomsWithUnreadMessagesByUserUuid(userUuid);
       }
 }
