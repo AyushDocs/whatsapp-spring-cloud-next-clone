41ea78c91844897613879345cead7e5c72909fb7
AyushDocs <ayushalokdubey@gmail.com>
2022-05-09 18:37:46 +0530

    fixed tests for image

--- images/src/main/java/com/whatsapp/images/controllers/ImageController.java
+++ images/src/main/java/com/whatsapp/images/controllers/ImageController.java
@@ -1,11 +1,12 @@
 package com.whatsapp.images.controllers;
 
+import static org.springframework.http.HttpStatus.ACCEPTED;
+
 import java.io.IOException;
 
 import com.whatsapp.images.configuration.ImageTypes;
 import com.whatsapp.images.services.ImageService;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,8 +24,8 @@ import lombok.RequiredArgsConstructor;
 public class ImageController {
     private final ImageService imageService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PostMapping("/")
+    @ResponseStatus(ACCEPTED)
     public void uploadImage(@RequestParam MultipartFile image, @RequestParam String userUuid) throws IOException{
         byte[] imageBytes = image.getBytes();
         imageService.storeProfileImage(imageBytes, userUuid);
--- images/src/test/java/com/whatsapp/images/controllers/ImageControllerTest.java
+++ images/src/test/java/com/whatsapp/images/controllers/ImageControllerTest.java
@@ -1,9 +1,12 @@
 package com.whatsapp.images.controllers;
 
 import static com.whatsapp.images.configuration.ImageTypes.PROFILE;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.verify;
-
-import java.io.ByteArrayInputStream;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.whatsapp.images.services.ImageService;
 
@@ -13,31 +16,50 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(ImageController.class)
 @ExtendWith(MockitoExtension.class)
 public class ImageControllerTest {
       @Autowired
       private ImageController imageController;
+      @Autowired
+      private MockMvc mvc;
       @MockBean
       private ImageService imageService;
-      
-      @Test
-      void should_upload_image() throws Exception{
-            byte[] bytes="image-file".getBytes();
-            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-            MultipartFile image=new MockMultipartFile("profile",bais);
 
-            imageController.uploadImage(image, "userUuid");
+      @Test
+      void controller_shoul_be_created() throws Exception {
+            assertNotNull(imageController);
+      }
+
+      @Test
+      void should_upload_image() throws Exception {
+            byte[] bytes = "image-file".getBytes();
+            ResultActions result = mvc.perform(multipart("/api/v1/images/")
+            .file("image",bytes)
+            .param("userUuid", "userUuid"));
 
             verify(imageService).storeProfileImage(bytes, "userUuid");
+            result.andExpect(status().isAccepted());
       }
-      @Test
-      void should_find_images() throws Exception{
-            imageController.findImage("userUuid", "PROFILE", "format");
 
-            verify(imageService).findImages(PROFILE, "format", "userUuid");
+      @Test
+      void should_find_images() throws Exception {
+            String image="chbuvbyfbcewiyfcbeyfvceyuv";
+            when(imageService.findImages(PROFILE, "jpg", "userUuid")).thenReturn(image);
+            MockHttpServletRequestBuilder request = get("/api/v1/images")
+            .param("userUuid", "userUuid")
+            .param("imageType", "profile")
+            .param("format", "jpg");
+
+            ResultActions result = mvc.perform(request);
+
+            verify(imageService).findImages(PROFILE, "jpg", "userUuid");
+            result.andExpect(status().isOk());
+            result.andExpect(MockMvcResultMatchers.content().string(image));
       }
 }
